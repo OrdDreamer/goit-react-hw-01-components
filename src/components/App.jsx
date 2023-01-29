5d3ae56f6592ba8d1fb206475a96871f2bbbe1d5
Roman Snitsarenko <59483945+OrdDreamer@users.noreply.github.com>
2023-01-29 16:36:25 +0200

    Task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import styles from './app.module.css'
+import ProfileCard from './ProfileCard';
+import userData from '../data/user.json';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
-        justifyContent: 'center',
+        flexDirection: 'column',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        gap: 50,
+        padding: 50,
+        color: '#010101',
+        backgroundColor: '#e7ecf2',
       }}
     >
-      React homework template
+      <ProfileCard
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+        className={styles.profile}
+      />
     </div>
   );
 };
---
+++ src/components/ProfileCard/index.jsx
@@ -0,0 +1,48 @@
+import styles from './profile-card.module.css';
+import PropTypes from 'prop-types';
+
+const ProfileCard = ({ username, tag, location, avatar, stats, className }) => {
+  return (
+    <div className={`${styles.card} ${className || ""}`}>
+      <div className={`${styles.infoContainer} ${avatar ? '' : styles.noImage}`}>
+        {avatar &&
+          <img
+            src={avatar}
+            alt={username}
+            width='200'
+            height='200'
+            className={styles.avatar}
+          />
+        }
+        <p className={styles.info}>
+          <span className={styles.userName}>{username}</span>
+          <span>@{tag}</span>
+          <span>{location}</span>
+        </p>
+      </div>
+      <ul className={styles.statsContainer}>
+        {Object.entries(stats).map((statsItem, index, array) => {
+          return (
+            <li key={statsItem[0]} className={styles.statsItem} style={{
+              flexBasis: `calc(100% / ${array.length})`,
+            }}>
+              <span>{statsItem[0]}</span>
+              <span className={styles.statsValue}>{statsItem[1]}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default ProfileCard;
+
+ProfileCard.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+  stats: PropTypes.objectOf(PropTypes.number),
+  className: PropTypes.string,
+};
