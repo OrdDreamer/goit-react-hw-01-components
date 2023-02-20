4cee32905532699b191c9e39f2484afe280546d0
Roman Snitsarenko <59483945+OrdDreamer@users.noreply.github.com>
2023-02-20 12:43:22 +0200

    Refactoring task 1.

--- src/components/ProfileCard/index.jsx
+++ src/components/ProfileCard/index.jsx
@@ -3,31 +3,30 @@ import PropTypes from 'prop-types';
 
 const ProfileCard = ({ username, tag, location, avatar, stats, className }) => {
   return (
-    <div className={`${styles.card} ${className || ""}`}>
-      <div className={`${styles.infoContainer} ${avatar ? '' : styles.noImage}`}>
+    <div className={`${styles.profile} ${className || ''}`}>
+      <div className={`${styles.description} ${avatar ? '' : styles.noImage}`}>
         {avatar &&
           <img
             src={avatar}
-            alt={username}
+            alt='User avatar'
             width='200'
             height='200'
             className={styles.avatar}
           />
         }
-        <p className={styles.info}>
-          <span className={styles.userName}>{username}</span>
-          <span>@{tag}</span>
-          <span>{location}</span>
-        </p>
+        <p className={styles.name}>{username}</p>
+        <p className={styles.tag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
       </div>
+
       <ul className={styles.stats}>
         {Object.entries(stats).map((statsItem, index, array) => {
           return (
             <li key={statsItem[0]} className={styles.statsItem} style={{
               flexBasis: `calc(100% / ${array.length})`,
             }}>
-              <span>{statsItem[0]}</span>
-              <span className={styles.statsValue}>{statsItem[1]}</span>
+              <span className={styles.label}>{statsItem[0]}</span>
+              <span className={styles.quantity}>{statsItem[1]}</span>
             </li>
           );
         })}
